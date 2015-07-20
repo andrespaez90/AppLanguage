7c36d6b1eec07e021d5e9c85e9ca3725e4251d0b
Andres Paez <andres.paez@innso.co>
2015-07-20 13:04:09 -0500

    Initial Commit

---
+++ AppLanguage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AppLanguage
+//
+//  Created by INNOVATING SOFTWARE SAS on 20/07/15.
+//  Copyright (c) 2015 INNOVATING SOFTWARE SAS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AppLanguage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AppLanguage
+//
+//  Created by INNOVATING SOFTWARE SAS on 20/07/15.
+//  Copyright (c) 2015 INNOVATING SOFTWARE SAS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
